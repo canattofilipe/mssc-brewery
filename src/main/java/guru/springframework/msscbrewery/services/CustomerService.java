52aa205686416eca4459fd98d272ab585bd28155
canattofilipe <canattofilipe@gmail.com>
2021-01-08 14:15:04 -0300

    add customer endpoint

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+public interface CustomerService {
+    CustomerDto getCustomerById(UUID uuid);
+}
