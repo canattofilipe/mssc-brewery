e53a9d6ec6cc9ce9543c73643df4df8a58b39844
canattofilipe <canattofilipe@gmail.com>
2021-01-15 17:58:39 -0300

    bean validation in CustomerDto

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -7,45 +7,64 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
 
-    private final CustomerService customerService;
+  private final CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+  public CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
 
-    @GetMapping({"/{customerId}"})
-    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID uuid){
-        return new ResponseEntity<>(customerService.getCustomerById(uuid), HttpStatus.OK) ;
-    }
+  @GetMapping({"/{customerId}"})
+  public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID uuid) {
+    return new ResponseEntity<>(customerService.getCustomerById(uuid), HttpStatus.OK);
+  }
 
-    @PostMapping
-    public ResponseEntity<CustomerDto> handlePost(@RequestBody CustomerDto customer){
-        CustomerDto savedDto =  customerService.saveNewCustomer(customer);
+  @PostMapping
+  public ResponseEntity<CustomerDto> handlePost(@Valid @RequestBody CustomerDto customer) {
+    CustomerDto savedDto = customerService.saveNewCustomer(customer);
 
-        HttpHeaders httpHeaders = new HttpHeaders();
+    HttpHeaders httpHeaders = new HttpHeaders();
 
-        // good practice / convention
-        httpHeaders.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
+    // good practice / convention
+    httpHeaders.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
 
-        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
-    }
+    return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+  }
 
-    @PutMapping("/{customerId}")
-    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customer){
-        customerService.updateCustomer(customerId, customer);
+  @PutMapping("/{customerId}")
+  public ResponseEntity handleUpdate(
+      @PathVariable("customerId") UUID customerId, @Valid @RequestBody CustomerDto customer) {
+    customerService.updateCustomer(customerId, customer);
 
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
+  }
 
-    @DeleteMapping("/{customerId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCustomer(@PathVariable("customerId") UUID customerId){
-        customerService.deleteById(customerId);
-    }
+  @DeleteMapping("/{customerId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+    customerService.deleteById(customerId);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+    List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+
+    e.getConstraintViolations()
+        .forEach(
+            constraintViolation -> {
+              errors.add(
+                  constraintViolation.getPropertyPath() + ":" + constraintViolation.getMessage());
+            });
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
 }
